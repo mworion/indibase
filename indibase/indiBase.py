fe048e5e37c07aab1bad0cf9e84db00dc3e31c73
mworion <michael.wuertenberger@t-online.de>
2019-08-04 18:10:36 +0200

    move to 5.13 and adding log statement

--- indibase/indiBase.py
+++ indibase/indiBase.py
@@ -244,7 +244,7 @@ class Client(PyQt5.QtCore.QObject):
                'setConnectionTimeout',
                ]
 
-    version = '0.100'
+    version = '0.101.0'
     logger = logging.getLogger(__name__)
 
     # INDI device types
