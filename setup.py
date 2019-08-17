a12a21cfc7582030d2935ce3e55c2a063c9fa75b
mworion <michael.wuertenberger@t-online.de>
2019-08-17 17:00:31 +0200

    got versions right

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
-# from distutils.core import setup
+############################################################
+# -*- coding: utf-8 -*-
+#
+#       #   #  #   #   #    #
+#      ##  ##  #  ##  #    #
+#     # # # #  # # # #    #  #
+#    #  ##  #  ##  ##    ######
+#   #   #   #  #   #       #
+#
+# Python-based Tool for interaction with the 10micron mounts
+# GUI with PyQT5 for python
+#
+# Michael Würtenberger
+# (c) 2019
+#
+# Licence APL2.0
+#
+###########################################################
 from setuptools import setup
-# from indibase.indiBase import Client
 
 indibase_version = '0.105'
 
