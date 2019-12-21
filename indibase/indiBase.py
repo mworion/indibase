a1366e28eaf1475dce3177f6bdcda52d60707f02
mworion <michael.wuertenberger@t-online.de>
2019-12-21 16:20:35 +0100

    removed error message when host is none

--- indibase/indiBase.py
+++ indibase/indiBase.py
@@ -54,8 +54,8 @@ class INDISignals(PyQt5.QtCore.QObject):
     defLight = PyQt5.QtCore.pyqtSignal(str, str)
 
     newMessage = PyQt5.QtCore.pyqtSignal(str, str)
-    serverConnected = PyQt5.QtCore.pyqtSignal(str)
-    serverDisconnected = PyQt5.QtCore.pyqtSignal(str)
+    serverConnected = PyQt5.QtCore.pyqtSignal()
+    serverDisconnected = PyQt5.QtCore.pyqtSignal(object)
     deviceConnected = PyQt5.QtCore.pyqtSignal(str)
     deviceDisconnected = PyQt5.QtCore.pyqtSignal(str)
 
@@ -303,7 +303,6 @@ class Client(PyQt5.QtCore.QObject):
     def checkFormat(self, value):
         # checking format
         if not value:
-            self.logger.error('wrong host value: {0}'.format(value))
             return None
         if not isinstance(value, (tuple, str)):
             self.logger.error('wrong host value: {0}'.format(value))
@@ -372,14 +371,14 @@ class Client(PyQt5.QtCore.QObject):
         if len(self._host) != 2:
             return False
         if self.connected:
-            self.signals.serverConnected.emit(self.host)
+            self.signals.serverConnected.emit()
             return True
         self.socket.connectToHost(*self._host)
         if not self.socket.waitForConnected(self.CONNECTION_TIMEOUT):
             self.connected = False
             return False
         self.connected = True
-        self.signals.serverConnected.emit(self.host)
+        self.signals.serverConnected.emit()
         return True
 
     def clearDevices(self, deviceName):
@@ -411,7 +410,7 @@ class Client(PyQt5.QtCore.QObject):
 
         self.connected = False
         self.clearParser()
-        self.signals.serverDisconnected.emit(list(deviceList.keys())[0])
+        self.signals.serverDisconnected.emit(self.devices)
         self.clearDevices(deviceName)
         self.socket.abort()
         return True
