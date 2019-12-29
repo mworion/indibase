bb57f84ff6f91050d95f53e332fb7bce75cd9d2a
mworion <michael.wuertenberger@t-online.de>
2019-12-29 14:25:13 +0100

    numpy to 1.18

--- setup.py
+++ setup.py
@@ -22,14 +22,14 @@ from setuptools import setup
 
 setup(
     name='indibase',
-    version='0.121',
+    version='0.122',
     packages=[
         'indibase',
     ],
     python_requires='>=3.6.0, <4.0',
     install_requires=[
         'PyQt5==5.13.2; platform_machine != "armv7l"',
-        'numpy==1.17.3',
+        'numpy==1.18.0',
     ],
     url='https://github.com/mworion/MountWizzard4',
     license='APL 2.0',
